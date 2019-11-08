27c8384cfca1a8ee578b637d71c417b54144f9f3
Ella Mathewson <ellaofohio@gmail.com>
2019-11-08 08:47:13 -0500

    Added an about page but nothing is working oof

---
+++ server/controllers/About.js
@@ -0,0 +1,16 @@
+// /* eslint-disable no-console */
+const models = require('../models');
+const Domo = models.Domo;
+
+const aboutPage = (req, res) => {
+  res.redirect('/about');
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
+  });
+};
+
+module.exports.aboutPage = aboutPage;
